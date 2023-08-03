9d649383b485e9d65c1559a816ca3aa461f04e5f
r1407p <buszter0403@gmail.com>
2023-08-03 22:11:24 +0800

    feat: contest information

--- te.py
+++
@@ -1,9 +0,0 @@
-x = 10  # global variable
-
-def my_function():
-    
-    #x = 20   # modifying the global variable 'x' inside the function
-    print(x)
-
-my_function()  # Output: 20
-print(x)       # Output: 20
