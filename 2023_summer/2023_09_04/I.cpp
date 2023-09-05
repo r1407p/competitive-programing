4ed4be63d1c3849af1e188c257dc8f28d3afd020
r1407p <buszter0403@gmail.com>
2023-09-05 18:45:20 +0800

    feat: upsolve I in NWERC 2017

--- 2023_summer/2023_09_04/I.cpp
+++ 2023_summer/2023_09_04/I.cpp
@@ -7,46 +7,52 @@ int main(){
     int n,c;
     cin >> n>>c;
     vector<vector<int>> nums;
-    for(int i =0;i<n;i++){
+    for(int i =1;i<=n;i++){
         int a,b;
         cin >> a>>b;
-        if(b>=a)a = b;
-        nums.push_back({b-a,a,b,i+1});
+        a  = max(a,b);
+        nums.push_back({b-a,a,b,i});
+        //0:sort 
+        //1:need 
+        //2:increase
+        //3:ind
     }
     sort(nums.begin(),nums.end());
-    vector<int>used(c+5,0);
-    int res = 0;
-    int ind = -1;
+    vector<vector<int>> dp(n+1,vector<int> (c+1,-1));
+    vector<vector<int>> from(n+1,vector<int> (c+1,-1));
+    vector<vector<int>> updated(n+1,vector<int> (c+1,-1));
+    //dp[0][j]is initial state;
+    dp[0][0] = 0;
     for(int i =0;i<n;i++){
-        for(int j =c-nums[i][1];j>=0;j--){
-            used[j+nums[i][2]] = max(used[j]+1,used[j+nums[i][2]]);
-            if(used[j+nums[i][2]]>res){
-                res = used[j+nums[i][2]];
-                ind = j+nums[i][2];
+        for(int j = 0;j<=c;j++){//keep last time record
+            dp[i+1][j] = dp[i][j];
+            from[i+1][j] = j;
+        }
+        for(int j =0;j<=c;j++){ // now is add nums[i]
+            if(dp[i][j]>=0){//avaliable to add
+                if(j+nums[i][1]<=c){//not exceed limit
+                    if(dp[i][j]+1>dp[i+1][j+nums[i][2]]){
+                        dp[i+1][j+nums[i][2]] = dp[i][j]+1;
+                        from[i+1][j+nums[i][2]] = j;
+                        updated[i+1][j+nums[i][2]] = nums[i][3];
+                    }
+                }
             }
         }
     }
-    int now = ind;
-    cout <<res<<endl;
-    if(res==0)return 0;
-    vector<int> tt;
-    while(used[now]!=0){
-        // cout <<now <<"!!!\n";
-        for(int i = n-1;i>=0;i--){
-            if(used[now]-1==used[now-nums[i][2]]&&now-nums[i][2]+nums[i][0]<=c){
-                tt.push_back(nums[i][3]);
-                now = now-nums[i][2];
-                continue;
-            }
+    int ind = 0;
+    for(int j = 0;j<=c;j++){
+        if(dp[n][j]>dp[n][ind])ind = j;
+    }
+    cout << dp[n][ind]<<endl;
+    vector<int> res;
+    for(int i = n;i>0;i--){
+        if(updated[i][ind]!=-1){
+            res.push_back(updated[i][ind]);
         }
+        ind = from[i][ind];
     }
-    for(int i = tt.size()-1;i>=0;i--){
-        cout <<tt[i]<<" ";
-    }
-    cout <<endl;
-
-
-
-
-
+    for(int i = res.size()-1;i>=0;i--){
+        cout <<res[i]<<' ';
+    }cout <<endl;
 }
